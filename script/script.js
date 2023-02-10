330f7b5ab0292c8d3badf31ddc4b2941df8c3382
Smetankin927 <55256885+Smetankin927@users.noreply.github.com>
2023-02-10 16:21:05 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 let profile = document.querySelector('.profile');
 let edit = profile.querySelector('.profile__edit-button');
 let nameProfile = profile.querySelector('.profile__name');
-let ProfProfile = profile.querySelector('.profile__name-subline');
+let profProfile = profile.querySelector('.profile__name-subline');
 
 let popup = document.querySelector(".popup");
 let popupContainer = popup.querySelector('.popup__container')
@@ -14,16 +14,17 @@ let popupInProf = popupContainer.querySelector('input[name = "profession"]');
 function showPopup() {
     popup.classList.add('popup_active');
     popupInName.value = nameProfile.textContent.trim();
-    popupInProf.value = ProfProfile.textContent.trim();
+    popupInProf.value = profProfile.textContent.trim();
 }
 
 function hidePopup(){
     popup.classList.remove('popup_active');
 }
 
-function updateProfile() {
+function updateProfile (evt) {
+    evt.preventDefault();
     nameProfile.textContent = popupInName.value.trim();
-    ProfProfile.textContent = popupInProf.value.trim();
+    profProfile.textContent = popupInProf.value.trim();
     hidePopup(); 
 }
 
@@ -32,4 +33,4 @@ function updateProfile() {
 
 edit.addEventListener('click', showPopup); 
 popupClose.addEventListener('click', hidePopup);
-popupSave.addEventListener('sumbit', updateProfile) /*здесь будет submit*/
+popupSave.addEventListener('submit', updateProfile); /*здесь будет submit*/
